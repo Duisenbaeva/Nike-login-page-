8bc6ac257703ff1c60cbe821c02dad26c3d5ce80
Diana <diana.duisenbaeva@icloud.com>
2025-11-29 23:41:52 +0500

    Initial Commit

---
+++ JustForMeTests/JustForMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustForMeTests.swift
+//  JustForMeTests
+//
+//  Created by diana on 29.11.2025.
+//
+
+import Testing
+@testable import JustForMe
+
+struct JustForMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
