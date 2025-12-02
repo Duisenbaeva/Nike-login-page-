13bcb65bb4e24216d46394e0ad7e958637483de0
Diana <diana.duisenbaeva@icloud.com>
2025-12-02 10:14:34 +0500

    Update LaunchScreen, Main, LoginCobtroller, SignInController

--- JustForMe/LoginController.swift
+++ JustForMe/LoginController.swift
@@ -7,23 +7,64 @@
 
 import UIKit
 
-class LoginController: UIViewController {
+class LoginController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var errorUsername: UILabel!
+    @IBOutlet weak var errorPassword: UILabel!
+    @IBOutlet weak var signButton: UIButton!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        errorPassword.isHidden = true
+        errorUsername.isHidden = true
+        
+        usernameField.delegate = self
+        passwordField.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func startPressed(_ sender: Any) {
+       
+        var isValid = true
+        if usernameField.text?.isEmpty == true {
+            errorUsername.isHidden = false
+            isValid = false
+            
+        } else {
+            errorUsername.isHidden = true
+        }
+        
+        if passwordField.text?.isEmpty == true {
+            errorPassword.isHidden = false
+            isValid = false
+            
+        } else {
+            errorPassword.isHidden = true
+        }
+    
+        if isValid {
+            openNextScreen(identifier: "ViewController")
+        }
     }
-    */
-
+    
+    @IBAction func signPressed(_ sender: UIButton) {
+        openNextScreen(identifier: "SignInController")
+    }
+    
+    func openNextScreen(identifier: String) {
+        print("Trying to open: \(identifier)")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextVC = storyboard.instantiateViewController(withIdentifier: identifier)
+        nextVC.modalPresentationStyle = .fullScreen
+        nextVC.modalTransitionStyle = .crossDissolve
+        present(nextVC, animated: true)
+        
+    }
+    
+    func textFieldShoulReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
--- JustForMe/SignInController.swift
+++ JustForMe/SignInController.swift
@@ -7,23 +7,85 @@
 
 import UIKit
 
-class SignInController: UIViewController {
+class SignInController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatTextField: UITextField!
+    @IBOutlet weak var errorName: UILabel!
+    @IBOutlet weak var errorEmail: UILabel!
+    @IBOutlet weak var errorPassword: UILabel!
+    @IBOutlet weak var errorRepeatPassword: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        errorName.isHidden = true
+        errorEmail.isHidden = true
+        errorPassword.isHidden = true
+        errorRepeatPassword.isHidden = true
+        
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate  = self
+        repeatTextField.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func createPressed(_ sender: Any) {
+        var isValid =  true
+        
+        if nameTextField.text?.isEmpty == true {
+            errorName.isHidden = false
+            isValid = false
+        } else {
+            errorName.isHidden = true
+        }
+        
+        if emailTextField.text?.isEmpty == true {
+            errorEmail.isHidden = false
+            isValid = false
+        } else {
+            errorEmail.isHidden = true
+        }
+        
+        if passwordTextField.text?.isEmpty == true {
+            errorPassword.isHidden = false
+            isValid = false
+        } else {
+            errorPassword.isHidden = true
+        }
+        
+        if repeatTextField.text?.isEmpty == true {
+            errorRepeatPassword.isHidden = false
+            isValid = false
+        } else {
+            errorRepeatPassword.isHidden = true
+        }
+        
+        if isValid {
+            openNextScreen(identifier: "ViewController")
+        }
     }
-    */
-
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        openNextScreen(identifier: "LoginController")
+    }
+    
+    func openNextScreen(identifier: String){
+        print("Trying to open: \(identifier)")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextVC = storyboard.instantiateViewController(withIdentifier: identifier)
+        nextVC.modalPresentationStyle = .fullScreen
+        nextVC.modalTransitionStyle = .crossDissolve
+        present(nextVC, animated: true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
